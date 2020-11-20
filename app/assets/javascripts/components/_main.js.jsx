77bab5ae8a2224d70126c6d2405db7fcd488c936
Loan Do <loan.do@thetrainline.com>
2020-11-20 14:53:11 +0100

    Homepage

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,7 @@
+const Main = (props) => {
+    return(
+        <div>
+            <h1>Glaçons are great!</h1>
+        </div>
+    )
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
+
+    root to: 'home#index'
     namespace :v1 do
       resources :glacons, only: [:index, :create, :destroy, :update]
     end
