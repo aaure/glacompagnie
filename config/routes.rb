d49eb893f4d4b6d4808cdfa5a2122bea05e1aba5
Loan Do <loan.do@thetrainline.com>
2020-11-20 15:01:48 +0100

    Third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
   namespace :api do
-
-    root to: 'home#index'
     namespace :v1 do
       resources :glacons, only: [:index, :create, :destroy, :update]
     end
