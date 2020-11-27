5f3969aaffc7fe5b15c0db73f88c4df2c76bd46c
Loan Do <loan.do@thetrainline.com>
2020-11-27 12:14:20 +0100

    delete react-rails gem

--- app/assets/javascripts/components/_main.js.jsx
+++
@@ -1,7 +0,0 @@
-const Main = (props) => {
-    return(
-        <div>
-            <h1>Glaçons are great!</h1>
-        </div>
-    )
-}
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
 glacons = ['Antarctique', 'Pôle Nord', 'Congélo', 'Penguin Special', 'Louis Vuitton']
+
 glacons.each {
     |glacon| Glacon.create(
       name: glacon,
